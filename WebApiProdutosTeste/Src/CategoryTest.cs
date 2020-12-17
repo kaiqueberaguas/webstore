99d6d7db23fd593a9a2654b64d8f34a7306489a6
Kaique Beraguas <ksakX@outlook.com>
2020-12-17 00:36:02 -0300

    Iniciando testes de unidade no dominio

---
+++ WebApiProdutosTeste/Src/CategoryTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApiProdutos.Src.Models;
+using Xunit;
+
+namespace WebApiProdutosTeste.Src
+{
+    public class CategoryTest
+    {
+
+        [Fact]
+        public void TestaUpdate()
+        {
+            //Criar objeto dinamico mocado 
+
+        }
+
+    }
+}
